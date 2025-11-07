795898bab633a207c820f41722b20b6aa909a839
Abraham Viera <viera.dev05@gmail.com>
2025-11-07 07:46:30 -0600

    Added currency and income fields
    Added a currency selection field with a combobox.
    For now, only USD and MXN, currencies I use.
    Also, added the income input field.
    In it, added some formating so that it always show at least 0.00
    and after adding more than 1,000 it adds a comma automatically.

--- ui_main.py
+++ ui_main.py
@@ -27,11 +27,61 @@ right_subframe.pack(side='right', fill='both', expand=True)
 
 #Creating lef side subframe data input fields. 
 #Date field
-ttk.Label(left_subframe, text='Date').grid(row=0,column=0,sticky='w',pady=5)
-date_entry = DateEntry(left_subframe, width=12, background='darkblue', foreground='white', borderwidth=2)
+ttk.Label(left_subframe, text='Date:').grid(row=0, column=0, sticky='w', pady=5)
+date_entry = DateEntry(left_subframe,
+                        width=12,
+                        background='darkblue',
+                        foreground='white', 
+                        borderwidth=2, 
+                        date_pattern = 'yyyy-mm-dd')
 date_entry.grid(row=0, column=1, pady=5, padx=5)
-selected_date = date_entry.get()
-print(selected_date)
+
+#Getting the date function
+def get_date():
+    selected_date = date_entry.get()
+#Button to test date function:
+#ttk.Button(left_subframe, text='Select date', command=get_date).grid(row=1, column=0, columnspan=1, pady=5)
+
+#Select currency field
+ttk.Label(left_subframe, text='Currency').grid(row=1, column=0, sticky='w', pady=5)
+currency_options = ['MXN', 'USD']
+currency_combo = ttk.Combobox(left_subframe, values=currency_options, state='readonly', width=5)
+currency_combo.grid(row=1, column=1, pady=5, padx=5, sticky='w')
+currency_combo.set('MXN') #default value
+
+
+
+#Creating the Income input field
+#Function that allow user to only input numbers and act similar to a cash register
+def format_input(event):
+    value = income_entry.get().replace('.', '').replace(',', '')
+    if event.char.isdigit():
+        value = (value + event.char)[-8:]
+    elif event.keysym == 'BackSpace':
+        value = value[:-1]
+    else:
+        return 'break'
+#Add the point automatically after cents
+    value = value.lstrip('0') or '0'
+    if len(value) <=3:
+        value = value.zfill(3) #Fill with 0 on the left of less than 3 digits including cents
+    formatted = f'{int(value[:-2]):,}.{value[-2:]}' 
+#Delete current input and replace with the new formatted one
+    income_entry.delete(0, tk.END)
+    income_entry.insert(0, formatted)
+    return 'break'
+
+#Field Label
+ttk.Label(left_subframe, text='Income: $').grid(row=2, column=0, sticky='e', pady=5)
+
+#Defining the income_entry variable
+income_entry = ttk.Entry(left_subframe, width=15, justify='right')
+income_entry.grid(row=2, column=1, padx=5, pady=5)
+income_entry.insert(0, '0.00')
+income_entry.bind('<KeyPress>', format_input)
+
+
+
 
 
 
@@ -43,3 +93,5 @@ print(selected_date)
 
 #Runs the mainloop (show main window)
 root.mainloop()
+
+ 
