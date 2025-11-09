c9940d2c4905cee00bc8524c70547aeb7cbbd08d
Abraham Viera <viera.dev05@gmail.com>
2025-11-09 10:38:03 -0600

    Added details box and preset combobox
    Added a .Text for the description field, as .Entry won't allow multiple
    lines.
    Also added a preset combobox on the right subframe. In it, user can
    choose different saving presets.
    Created the default.json file, where presets are read from.
    Made ui_main.py read the default.json file.
    Finally, added a "show_categories" function that .destroy the labels.
    Still pending on the funtion to create the new lables.
    From now on, won't be able to work in the proyect for a while as I have
    1 month left for my JLPT N1 test (December 7, 2025)

--- ui_main.py
+++ ui_main.py
@@ -4,6 +4,15 @@ from tkinter import ttk
 from tkinter import messagebox
 from tkcalendar import DateEntry
 
+#Added defalut.json database for saving presets
+import json
+import os
+#Path to my preset file
+preset_path = os.path.join('presets', 'default.json')
+#Load the presets
+with open(preset_path, 'r') as preset_file:
+    preset_data = json.load(preset_file)
+
 #Setting up root window (main window)
 root = tk.Tk()
 root.title('Viera\'s Personal Savings App')
@@ -17,24 +26,24 @@ title_label.pack(pady=10)
 
 #Creating sub frames. 
 #Left side subframe: data entry like date, income, description
-left_subframe = ttk.Frame(main_frame, padding=20)
+left_subframe = ttk.Frame(main_frame, padding=5)
 left_subframe.pack(side='left', fill='both', expand=True)
 
 #Rifht side subframe: data entry for categories, like rent, food, hobbies and percentages per each one.
-right_subframe = ttk.Frame(main_frame, padding=20)
+right_subframe = ttk.Frame(main_frame, padding=5)
 right_subframe.pack(side='right', fill='both', expand=True)
 
 
 #Creating lef side subframe data input fields. 
 #Date field
-ttk.Label(left_subframe, text='Date:').grid(row=0, column=0, sticky='w', pady=5)
+ttk.Label(left_subframe, text='Date:').grid(row=0, column=0, sticky='e', pady=5)
 date_entry = DateEntry(left_subframe,
                         width=12,
                         background='darkblue',
                         foreground='white', 
                         borderwidth=2, 
                         date_pattern = 'yyyy-mm-dd')
-date_entry.grid(row=0, column=1, pady=5, padx=5)
+date_entry.grid(row=0, column=1, sticky='w', pady=5, padx=5)
 
 #Getting the date function
 def get_date():
@@ -43,7 +52,7 @@ def get_date():
 #ttk.Button(left_subframe, text='Select date', command=get_date).grid(row=1, column=0, columnspan=1, pady=5)
 
 #Select currency field
-ttk.Label(left_subframe, text='Currency').grid(row=1, column=0, sticky='w', pady=5)
+ttk.Label(left_subframe, text='Currency:').grid(row=1, column=0, sticky='e', pady=5)
 currency_options = ['MXN', 'USD']
 currency_combo = ttk.Combobox(left_subframe, values=currency_options, state='readonly', width=5)
 currency_combo.grid(row=1, column=1, pady=5, padx=5, sticky='w')
@@ -76,14 +85,49 @@ ttk.Label(left_subframe, text='Income: $').grid(row=2, column=0, sticky='e', pad
 
 #Defining the income_entry variable
 income_entry = ttk.Entry(left_subframe, width=15, justify='right')
-income_entry.grid(row=2, column=1, padx=5, pady=5)
+income_entry.grid(row=2, column=1, padx=5, pady=5, sticky='w')
 income_entry.insert(0, '0.00')
 income_entry.bind('<KeyPress>', format_input)
 
 
+#Description field
+ttk.Label(left_subframe, text='Income Details:').grid(row=3, column=0, columnspan=2, sticky='w',pady=5)  
+description_text = tk.Text(left_subframe, width=20, height=5, wrap='word')
+#Used .Text instead of .Entry because Entry won't allow height option
+description_text.grid(row=4, column=0, columnspan=2, sticky='w', pady=5)
 
 
+#Right subframe fields
+#Need to think of some things before coding this section (november 8th)
+#Categories must support nesting and dynamic changes (adding or deleting a categorie)
+#Should validate that percentages won't exceed the parent percentage
+#Should be able to save/load categorie presets
+#Need to be careful of extensibility for future charting or allow multiple profiles
+#Scroll support if added a lot of categories
 
+ttk.Label(right_subframe, text='Preset:').grid(row=0, column=0, pady=5, sticky='w')
+preset_names = [preset_data['preset_name']]
+#Selecting the preset from a combobox thats read from default.json
+preset_combo = ttk.Combobox(right_subframe, values = preset_names, state='readonly', width=15)
+preset_combo.grid(row=0, column=1, pady=5, sticky='w')
+preset_combo.set(preset_names[0]) #set default
+
+#Select preset function
+def preset_selected(event):
+    selected_name = preset_combo.get()
+preset_combo.bind('<<ComboboxSelected>>', preset_selected)
+
+#Function for dynamic labels depending on the preset.
+category_labels = []
+
+def show_categories(categories):
+    #First clear labels
+    for i in category_labels:
+        i.destroy() #need to destroy each label, one by one
+    category_labels.clear() #then clears the list
+
+    #add new labels 
+    
 
 
 
